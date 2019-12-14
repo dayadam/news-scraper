e97746851d95f6cbd272207d5ecedd0891fe55e6
dayadam <dayadamuga@gmail.com>
2019-12-13 19:51:09 -0500

    server built out

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const PORT = 3000;
+const app = express();
+
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Make public a static folder
+app.use(express.static("public"));
+
+// Connect to the Mongo DB
+mongoose.connect("mongodb://localhost/newsScrapeDB", { useNewUrlParser: true });
+
+// Start the server
+app.listen(PORT, function() {
+  console.log("App running on port " + PORT + "!");
+});
