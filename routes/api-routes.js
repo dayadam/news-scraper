11abacecda3d9d1c5a270ddc0f3d9cbf3b82f420
dayadam <dayadamuga@gmail.com>
2019-12-14 22:46:12 -0500

    article model and schema added

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,34 +1,28 @@
-const db = require("../models/Articles.js");
 const cheerio = require("cheerio");
 const axios = require("axios");
 
 // Routes
 // =============================================================
-module.exports = function(app) {
+module.exports = function(app, db) {
   app.get("/", function(req, res) {
     axios.get("https://hypepotamus.com/").then(function(response) {
       const $ = cheerio.load(response.data);
       const results = [];
       $(".entry-title").each(function(i, element) {
-        const title = $(this)
+        const result = {};
+        result.title = $(this)
           .children("a")
           .text();
-        const link = $(this)
+        result.link = $(this)
           .children("a")
           .attr("href");
-        const snippet = $(this)
+        result.snippet = $(this)
           .parent()
           .next()
           .children("p")
           .text();
-        const result = {
-          title: title,
-          link: link,
-          snippet: snippet
-        };
         results.push(result);
       });
-      console.log(results)
       res.render("index", { articles: results });
     });
   });
--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const exphbs = require("express-handlebars");
-
-const PORT = 3000;
+const mongoose = require("mongoose");
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 // Parse request body as JSON
@@ -10,13 +9,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/newsScrapeDB", { useNewUrlParser: true });
 // Make public a static folder
 app.use(express.static("public"));
+const db = require("../models/Articles.js");
 
-// Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/newsScrapeDB", { useNewUrlParser: true });
-
-require("./routes/api-routes.js")(app);
+require("./routes/api-routes.js")(app, db);
 
 // Start the server
 app.listen(PORT, function() {
